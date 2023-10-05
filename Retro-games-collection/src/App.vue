17ac9db6ba3dad3f9b6c89b7159142aff8b2d4f4
rmf2pt <rmf2pt@gmail.com>
2023-10-05 22:45:08 +0100

    Create new GreetingMsg SFC and tests to validate
    Signed-off-by: rmf2pt <rmf2pt@gmail.com>

--- Retro-games-collection/src/App.vue
+++ Retro-games-collection/src/App.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
+import GreetingMsg from '@/components/GreetingMsg.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    GreetingMsg
+  }
+})
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <greetingMsg></greetingMsg>
     </div>
+    <p>This is first paragraph</p>
   </header>
 
   <main></main>
@@ -16,10 +25,10 @@ import HelloWorld from './components/HelloWorld.vue'
 header {
   display: flex;
   place-items: center;
+  flex-flow: column;
 }
 
 .wrapper {
-  display: flex;
-  place-items: center;
+  display: block;
 }
 </style>
---
+++ Retro-games-collection/src/components/GreetingMsg.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'GreetingMsg',
+  data() {
+    return {
+      msg: 'Hello World'
+    }
+  },
+  computed: {
+    greeting(): string {
+      return this.msg + '!'
+    }
+  }
+})
+</script>
+
+<template>
+  <p>{{ greeting }}</p>
+</template>
+
+<style scoped>
+p {
+  color: rgb(255, 0, 0);
+}
+</style>
