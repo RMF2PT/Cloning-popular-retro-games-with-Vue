9e70a249082726922e96dae38a23f543a276f2f5
rmf2pt <rmf2pt@gmail.com>
2023-10-07 20:40:03 +0100

    Add Game Details component
    Signed-off-by: rmf2pt <rmf2pt@gmail.com>

---
+++ Retro-games-collection/src/components/views/GameDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>This is the page for {{ game?.name }}</h1>
+    <img :src="game?.image" alt="" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import sourceData from '@/games.json'
+
+export default defineComponent({
+  name: 'GameDetails',
+  computed: {
+    game() {
+      return sourceData.games.find((game) => game.id === Number(this.$route.params.id))
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/_variables.scss';
+</style>
--- Retro-games-collection/src/router/index.ts
+++ Retro-games-collection/src/router/index.ts
@@ -2,13 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Home from '@/components/views/HomeCmp.vue'
 import GameCollection from '@/components/views/GameCollection.vue'
+import GameDetails from '@/components/views/GameDetails.vue'
 import About from '@/components/views/AboutCmp.vue'
-
 import NotFound from '@/components/views/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/games', name: 'Games Collection', component: GameCollection },
+  { path: '/games/:id', component: GameDetails },
   { path: '/about', name: 'About', component: About },
   { path: '/:catchAll(.*)', name: 'Not found', component: NotFound }
 ]
